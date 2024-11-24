2d289284eb572d238a3b405d85bda49373a05e52
SultanMachido <opehardesina@gmail.com>
2024-11-24 01:15:48 -0800

    add config file

--- STATIC_API/Config.php
+++ STATIC_API/Config.php
@@ -17,7 +17,7 @@
     $dbname = "prosecare";
     
     // $SITE_URL = "https://prosecare.com/TEST_V2";
-    $SITE_URL = "https://renderer.endor.dev/app"
+    $SITE_URL = "https://renderer.endor.dev/app";
 
     $VIDEO_CONFERENCING_URL = 'https://6251-102-117-182-61.ngrok-free.app/';
 
